eb4ea4bab86365c638ec6d72c0bb52d1ab4e1d43
Ehab R. Barham <ebarham@stcs.com.sa>
2022-09-03 02:44:07 +0300

    circleci: lint

--- src/test/java/ehab/udacitycapstone/UdacityCapstoneApplicationTests.java
+++ src/test/java/ehab/udacitycapstone/UdacityCapstoneApplicationTests.java
@@ -1,13 +1,25 @@
 package ehab.udacitycapstone;
 
+import ehab.udacitycapstone.controller.HomeController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class UdacityCapstoneApplicationTests {
 
+    HomeController homeController;
+
+    @BeforeEach
+    void setUp(){
+        homeController = new HomeController();
+    }
+
     @Test
     void contextLoads() {
+        assertTrue(homeController.Home().equals("App is up"));
     }
 
 }
